9677eb7824df07150928a0354be8813fdee4fec0
waelanwar1 <waelanwar.etx@gmail.com>
2024-11-08 01:13:16 +0200

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName:'DayUse',
+        productNumber:'S001',
+        paymentStatus:'paid',
+        shipping:'declined'
+    },
+    {
+        productName:'hotel res.',
+        productNumber:'S002',
+        paymentStatus:'unpaid',
+        shipping:'delivred'
+    },
+    {
+        productName:'rest. res.',
+        productNumber:'S003',
+        paymentStatus:'paid',
+        shipping:'delivred'
+    },
+    {
+        productName:'parking',
+        productNumber:'S004',
+        paymentStatus:'paid',
+        shipping:'pending'
+    },
+    {
+        productName:'hotel res.',
+        productNumber:'S002',
+        paymentStatus:'paid',
+        shipping:'pending'
+    },
+    {
+        productName:'parking',
+        productNumber:'S004',
+        paymentStatus:'paid',
+        shipping:'delivred'
+    }
+]
